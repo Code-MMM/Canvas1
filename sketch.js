4b8b12921aa5f16e940f5ff6b37d6928a78db3e3
Code-MMM <63055277+Code-MMM@users.noreply.github.com>
2020-05-28 14:31:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var button, thickness, plusButton, minusButton, confirmButton, colorInput, color;
+var button, thickness, plusButton, minusButton, confirmButton, colorInput, color, message;
 thickness = 10;
 color = "black"
 
@@ -8,7 +8,7 @@ function setup() {
     button = createButton("Clear Canvas")
     button.position((displayWidth - 50)/2, displayHeight - 250)
 
-    colorInput = createInput("Color")
+    colorInput = createInput("black")
     confirmButton = createButton("Set Color")
 
     plusButton = createButton("+ thickness")
@@ -17,6 +17,12 @@ function setup() {
     plusButton.position(((displayWidth - 50)/2) + 115, displayHeight - 250)
 
     minusButton.position(((displayWidth - 50)/2) - 100, displayHeight - 250)
+
+    message = createElement('h2')
+
+    message.position(((displayWidth - 50)/2) - 130, 30)
+
+    message.html("Click Set Color to start drawing!")
 }
 
 function draw() {
